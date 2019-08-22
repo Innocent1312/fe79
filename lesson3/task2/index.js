a5f23129a2b60c6559f9f4b77c4ce902f19a065e
Innocent1312 <anthonydev33@gmail.com>
2019-08-22 14:45:06 +0300

    small beauty

--- lesson3/task2/index.js
+++ lesson3/task2/index.js
@@ -5,8 +5,8 @@ create_btn.addEventListener('click', () => {
     let p = document.createElement('p');
     p.innerHTML = 'Paragraph'
     div1.appendChild(p)
-    if (div1.children.length > 10){
-        while(div1.firstChild){
+    if (div1.children.length > 10) {
+        while (div1.firstChild) {
             div1.removeChild(div1.firstChild)
         }
     }
